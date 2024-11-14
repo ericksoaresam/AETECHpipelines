33ebea788e9b6375354b53dd7194b1e676e63175
Pc Andrews <Pc Andrews@DESKTOP-FBOM62Q>
2024-11-14 00:52:48 -0300

    Adicionar arquivos de projeto.

---
+++ EnergyAPI/Controllers/EnergyController.cs
@@ -0,0 +1,33 @@
+﻿using EnergyAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnergyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnergyController : ControllerBase
+    {
+        private readonly OracleDbContext _dbContext;
+
+        public EnergyController(OracleDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost("usuario")]
+        public IActionResult InserirUsuario(string nome, string email, string senha, string cpf)
+        {
+            _dbContext.InsertUsuario(nome, email, senha, cpf);
+            return Ok("Usuário inserido com sucesso!");
+        }
+
+        [HttpPost("dispositivo")]
+        public IActionResult InserirDispositivo(int idUsuario, string nomeDispositivo, string tipo, string localizacao)
+        {
+            _dbContext.InsertDispositivo(idUsuario, nomeDispositivo, tipo, localizacao);
+            return Ok("Dispositivo inserido com sucesso!");
+        }
+
+        // Adicione outros métodos para as outras procedures
+    }
+}
---
+++ EnergyAPI/Data/OracleDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Data;
+
+namespace EnergyAPI.Data
+{
+    public class OracleDbContext : IDisposable
+    {
+        private readonly OracleConnection _connection;
+
+        public OracleDbContext(IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("OracleDB");
+            _connection = new OracleConnection(connectionString);
+            _connection.Open();
+        }
+
+        public void Dispose()
+        {
+            _connection.Close();
+        }
+
+        public void InsertUsuario(string nome, string email, string senha, string cpf)
+        {
+            using var command = new OracleCommand("sp_insert_usuario", _connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("p_nome", OracleDbType.Varchar2).Value = nome;
+            command.Parameters.Add("p_email", OracleDbType.Varchar2).Value = email;
+            command.Parameters.Add("p_senha", OracleDbType.Varchar2).Value = senha;
+            command.Parameters.Add("p_cpf", OracleDbType.Varchar2).Value = cpf;
+            command.ExecuteNonQuery();
+        }
+
+        public void InsertDispositivo(int idUsuario, string nomeDispositivo, string tipo, string localizacao)
+        {
+            using var command = new OracleCommand("sp_insert_dispositivo", _connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("p_id_usuario", OracleDbType.Int32).Value = idUsuario;
+            command.Parameters.Add("p_nome_dispositivo", OracleDbType.Varchar2).Value = nomeDispositivo;
+            command.Parameters.Add("p_tipo", OracleDbType.Varchar2).Value = tipo;
+            command.Parameters.Add("p_localizacao", OracleDbType.Varchar2).Value = localizacao;
+            command.ExecuteNonQuery();
+        }
+
+        // Adicione os outros métodos para as outras stored procedures aqui...
+    }
+}
